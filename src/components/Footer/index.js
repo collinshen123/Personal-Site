97de4328b7d495a653e3f66abdcac7406aed1f41
collinshen123 <collinshen123@gmail.com>
2024-01-18 14:31:45 -0800

    changed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -105,7 +105,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Collin Shen. All rights reserved.
+          &copy; 2024 Collin Shen. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
